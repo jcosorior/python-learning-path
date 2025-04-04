7ba0909f822980c9ed5da5f07e27aecf66c84ba2
jcosorior <juanca8014@hotmail.com>
2025-04-04 17:24:47 -0500

    sum of all entered numbers exercise in progress

--- pythonProject/while_and_for_excercises/count_1_to_10.py
+++ pythonProject/while_and_for_excercises/count_1_to_10.py
@@ -1,9 +1,19 @@
+#this first way it was my way to resolve the excersice 
 import time
-num = 0 
-while True:
-    if num != 10:
-        num = num + 1
-        print(num)
-        time.sleep(0.5)
-    else:
-        break
+#num = 0 
+#while True:
+#    if num != 10:
+#        num = num + 1
+#        print(num)
+#        time.sleep(0.5)
+#    else:
+#        break
+
+
+#this second version is a sugested version that a senior friend gave me, he says there is some good practices and for this case this excersice is better
+# I'm not familiar with complexity of good practice yet, so I'm going to keep going through my learning process
+num = 0
+while num < 10:
+    num +=1
+    print(num)
+    time.sleep(0.5)
---
+++ pythonProject/while_and_for_excercises/sum_of_all_entered_numbers.py
@@ -0,0 +1,4 @@
+num = int(input("Enter your number to start adding, once you are finished enter 0"))
+while num != 0:
+    num = int("keep adding")
+print(num)
