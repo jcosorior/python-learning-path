5509d0e0ff74ff56dc70bf50a13bd4c0bef950ac
jcosorior <juanca8014@hotmail.com>
2025-04-11 15:45:55 -0500

    I want to ad a limit of 3 attemps to make it more realistic

--- pythonProject/while_and_for_excercises/ATM_simulation.py
+++ pythonProject/while_and_for_excercises/ATM_simulation.py
@@ -1,7 +1,13 @@
 
 passcode = int(input("Enter your passcode: "))
-
+attemps = 3
 while passcode !=1234:
     passcode = int(input("Try again: "))
-    
-print("Transaction in progress")
+    if passcode != 1234:
+        attemps -= 1
+    else:
+        break    
+if attemps == 0:
+    print("Your card is block, call your bank")
+else:
+    print("Transaction in progress")
